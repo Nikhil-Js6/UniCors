884feede4f57c36efdf0f588687d540605315293
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-07-15 11:20:28 +0530

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,156 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+class AuthController {
+
+    async register (req, res) {
+
+        const { name, email, password, secret, answer: secret_key } = req.body;
+
+        User.findOne({email}).exec((err, user) => {
+            if (user) {
+                return res.status(400).json({
+                    error: 'Email is already Taken',
+                })
+            }
+            console.log(err);
+
+            if(!secret || !secret_key) {
+                return res.status(400).json({
+                    error: 'Please pick a question',
+                })
+            }
+
+        //Generate hashed password:
+            const hashed_password = bcrypt.hashSync(password, 12);
+
+        // Create new user:
+            const newUser = new User({ name, email, hashed_password, secret, secret_key });
+
+            newUser.save((err, user) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(401).json({
+                        error: 'Error saving the user!'
+                    })
+                }
+                return res.status(201).json({
+                    user,
+                    message: 'Registration Success. Please login'
+                })
+            })
+        })
+    }
+
+    async login (req, res) {
+
+        const { email, password } = req.body;
+
+        try {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({
+                    error: 'User doesn\'t exist!'
+                });
+            }
+
+            const validPassword = bcrypt.compareSync(password, user.hashed_password);
+            if (!validPassword) { 
+                return res.status(400).json({
+                    error: 'Incorrect Password!'
+                });
+            }
+
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+            const { hashed_password, secret, secret_key, ...userInfo } = user._doc;
+
+            return res.status(200).json({
+                message: 'Logged in successfully',
+                userInfo,
+                token
+            });
+        } 
+        catch (err) {
+            console.log(err);
+            return res.status(400).json({
+                error: 'Server error. Try again!'
+            });
+        }
+    }
+
+    async verifyUser (req, res) {
+        const userId = req.user._id;
+        try {
+            const user = await User.findById(userId);
+            if (user) {
+                return res.status(200).json({ verified: true });
+            }
+        }
+        catch (err) {
+            return res.status(500).json({
+                error: 'Can\'t verify user!',
+                verified: false
+            });
+        }
+    }
+
+    async forgotPassword (req, res) {
+
+        const { email, newPassword, secret, answer: secret_key } = req.body;
+
+        if (!email) {
+            return res.status(400).json({
+                error: 'Enter your email'
+            })
+        }
+
+        if (!newPassword || newPassword.length < 6) {
+            return res.status(400).json({
+                error: 'Enter new password'
+            })
+        }
+
+        if (!secret || !secret_key) {
+            return res.status(400).json({
+                error: 'Please pick a question and answer',
+            })
+        }
+
+        const user = await User.findOne({ email, secret });
+
+        if (!user) {
+            return res.status(400).json({
+                error: 'Can\'t find the user, Choose correct details',
+            })
+        }
+
+        try {
+            const hashed_password = bcrypt.hashSync(newPassword, 12);
+            await User.findByIdAndUpdate(
+                user._id, 
+                { hashed_password }, 
+                { new: true }
+            );
+
+            return res.status(200).json({
+                message: 'Password reset success. Login with new password.'
+            })
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(401).json({
+                error: 'Error saving the user!'
+            })
+        } 
+    }
+
+    async profileUpdate (req, res) {
+        console.log(req.body);
+    }
+}
+
+module.exports = new AuthController();
