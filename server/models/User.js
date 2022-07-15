7b96f91c95b6a60e3ab54b37a445493e5561ce41
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-07-15 11:17:23 +0530

    Add files via upload

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 20
+    },
+    about: {
+
+    },
+    secret: {
+        type: String,
+        required: true
+    },
+    secret_key: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    followings: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "User"
+        }
+    ],
+    followers: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "User"
+        }
+    ]
+},
+
+    { timestamps: true }
+
+);
+
+module.exports = new mongoose.model("User", userSchema);
