08fa1f0dd4bad757aac5c62b13e092c86f55407d
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-08-03 15:16:22 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,17 @@ const User = require('../models/User');
 
 class UserController {
 
+    async getUser (req, res) {
+        try {
+            const user = await User.findById(req.body._id)
+               .select('name image followers followings about');
+            return res.status(200).json(user);
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
+
     async findPeople (req, res) {
         try {
             const user = await User.findById(req.user._id);
@@ -10,14 +21,14 @@ class UserController {
 
             const people = await User.find({ _id: { $nin: followings }})
             .select('-hashed_password -secret -secret_key').limit(6);
-
-//****OR ****/
-
+            
+     //****OR ****/
             // const people = await User.find({ _id: { $nin: followings }}).limit(6);
             // const trimmed = people.map((person) => {
             //     const { name, image, _id } = person._doc;
             //     return { name, image, _id };
             // })
+            
             res.json(people);
         }
         catch (err) {
@@ -28,8 +39,13 @@ class UserController {
 
     async addFollower (req, res, next) {
         try {
-            const user = await User.findByIdAndUpdate({ _id: req.body._id }, {
-                $addToSet : { followers: req.user._id }
+            if (req.user._id === req.body._id) {
+                return res.json({ 
+                    error: 'Can\'t follow yourself!'
+                });
+            }
+            await User.findByIdAndUpdate({ _id: req.body._id }, {
+                $addToSet: { followers: req.user._id }
             });
             next();
         }
@@ -38,7 +54,7 @@ class UserController {
         }
     }
 
-    async userFollowing (req, res) {
+    async addUserFollowing (req, res) {
         try {
             const user = await User.findByIdAndUpdate({ _id: req.user._id },
             {
@@ -55,6 +71,138 @@ class UserController {
             return res.status(500).json('Can\'t follow!');
         }
     }
+    
+    async removeFollower (req, res, next) {
+        try {
+            await User.findByIdAndUpdate({ _id: req.body._id }, {
+                $pull: { followers: req.user._id }
+            });
+            next();
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async removeUserFollowing (req, res) {
+        try {
+            const user = await User.findByIdAndUpdate({ _id: req.user._id },
+            {
+                $pull: { followings: req.body._id }
+            },
+                { new: true }
+            )
+            .select('-hashed_password -secret -secret_key');
+
+            return res.status(200).json(user);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(500).json('Can\'t follow!');
+        }
+    }
+
+    async userFollowings (req, res) {
+        try {
+            const user = await User.findById(req.user._id);
+            const followings = await User.find({ _id: user.followings })
+            .select('-email -hashed_password -secret -secret_key');
+
+            return res.status(200).json(followings);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(500).json('Can\'t find followings!');
+        }
+    }
+
+    async userFollowers (req, res) {
+        try {
+            const user = await User.findById(req.user._id);
+            const followers = await User.find({ _id: user.followers })
+            .select('-email -hashed_password -secret -secret_key');
+
+            return res.status(200).json(followers);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(500).json('Can\'t find followings!');
+        }
+    }
+
+    async removeFollowing (req, res, next) {
+        try {
+            await User.findByIdAndUpdate({ _id: req.body._id }, {
+                $pull: { followings: req.user._id }
+            });
+            next();
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async removeUserFollower (req, res) {
+        try {
+            const user = await User.findByIdAndUpdate({ _id: req.user._id }, 
+            {
+                $pull: { followers: req.body._id }, 
+            },
+                { new: true }
+            )
+            .select('-hashed_password -secret -secret_key');
+
+            return res.status(200).json(user);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(500).json('Can\'t remove!');
+        }
+    }
+
+    async searchUser (req, res) {
+        const { query } = req.params;
+        if (!query) return;
+        try {
+            const user = await User.find({
+                $or: [
+                    { name: { $regex: query, $options: 'i' }},
+                    { username: { $regex: query, $options: 'i' }},
+                ]
+            })
+               .select('name image _id followers');
+            res.status(200).json(user);
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getFollowers (req, res) {
+        try {
+            const user = await User.findById(req.body._id);
+            const followers = await User.find({ _id: user.followers })
+               .select('-email -hashed_password -secret -secret_key');
+
+            return res.status(200).json({ user: user.name, followers });
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getFollowings (req, res) {
+        try {
+            const user = await User.findById(req.body._id);
+            const followings = await User.find({ _id: user.followings })
+               .select('-email -hashed_password -secret -secret_key');
+
+            return res.status(200).json({ user: user.name, followings });
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    } 
 }
 
 module.exports = new UserController();
