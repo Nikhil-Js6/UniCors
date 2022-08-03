127f5b4213a902554cbcfd31c6fd9eef9cab98ff
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-08-03 15:23:22 +0530

    Update post.js

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,10 +3,25 @@ const formidable = require('express-formidable');
 
 const verify = require('../middlewares/verifyToken');
 
-const { createPost, uploadImage, userPosts } = require('../controllers/postController');
+const { createPost, 
+        uploadImage, 
+        userPosts, 
+        userFeed, 
+        likePost, 
+        unlikePost, 
+        addComment, 
+        removeComment } = require('../controllers/postController');
 
 router.post('/create-post', verify, createPost);
 router.post('/upload-image', verify, formidable({ maxFileSize: 5 * 1024 * 1024 }), uploadImage);
-router.get('/user-posts', verify, userPosts);
+
+router.post('/user-posts', userPosts);
+router.get('/user-feed', verify, userFeed);
+
+router.put('/like-post', verify, likePost);
+router.put('/unlike-post', verify, unlikePost);
+
+router.put('/add-comment', verify, addComment);
+router.put('/remove-comment', verify, removeComment);
 
 module.exports = router;
