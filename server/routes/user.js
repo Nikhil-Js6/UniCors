660e1f1824d620a997ad87be154117f9a37ddc89
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-08-03 15:14:19 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,34 @@ const router = require('express').Router();
 
 const verify = require('../middlewares/verifyToken');
 
-const { findPeople, addFollower, userFollowing } = require('../controllers/userController');
+const { getUser,
+        findPeople, 
+        addFollower, 
+        addUserFollowing, 
+        userFollowings, 
+        userFollowers, 
+        removeFollower, 
+        removeUserFollowing, 
+        removeFollowing, 
+        removeUserFollower,
+        searchUser,
+        getFollowers,
+        getFollowings } = require('../controllers/userController');
 
-router.get('/find-people', verify, findPeople);
-router.put('/follow-user', verify, addFollower, userFollowing );
+router.post('/get-user', verify, getUser );
+
+router.get('/find-people', verify, findPeople );
+router.put('/follow-user', verify, addFollower, addUserFollowing );
+
+router.get('/user-followings', verify, userFollowings );
+router.put('/unfollow-user', verify, removeFollower, removeUserFollowing );
+
+router.get('/user-followers', verify, userFollowers );
+router.put('/remove-follower', verify, removeFollowing, removeUserFollower );
+
+router.get('/search-user/:query', searchUser );
+
+router.post('/get-followers', getFollowers );
+router.post('/get-followings', getFollowings );
 
 module.exports = router;
