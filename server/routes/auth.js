57a9f41d70f5461527c7755bf6cb5b9b2d4cbfcf
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-08-05 22:31:05 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,21 @@ const router = require('express').Router();
 
 const verify = require('../middlewares/verifyToken');
 
-const { register, login, verifyUser, forgotPassword, profileUpdate } = require('../controllers/authController');
+const { register, 
+        login, 
+        verifyUser, 
+        verifyAdmin, 
+        forgotPassword, 
+        profileUpdate 
+    } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/verify-user', verify, verifyUser);
 router.post('/forgot-password', forgotPassword );
 router.put('/profile-update', profileUpdate );
+
+// Admin: 
+router.get('/verify-admin', verify, verifyAdmin);
 
 module.exports = router;
