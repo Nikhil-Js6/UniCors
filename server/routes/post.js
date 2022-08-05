79f0f09e0fcc151a802e0fd365dfc7ff08b05f30
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-08-05 22:38:53 +0530

    Update post.js

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,26 +2,38 @@ const router = require('express').Router();
 const formidable = require('express-formidable');
 
 const verify = require('../middlewares/verifyToken');
+const { verifyAdmin } = require('../middlewares/verifyUser');
 
 const { createPost, 
         uploadImage, 
-        userPosts, 
+        getPost,
+        deletePost,
         userFeed, 
+        userPosts,
         likePost, 
         unlikePost, 
         addComment, 
-        removeComment } = require('../controllers/postController');
+        removeComment,
+        allPosts } = require('../controllers/postController');
 
 router.post('/create-post', verify, createPost);
 router.post('/upload-image', verify, formidable({ maxFileSize: 5 * 1024 * 1024 }), uploadImage);
 
-router.post('/user-posts', userPosts);
+router.get('/post/:id', verify, getPost);
+router.delete('/post/:id', verify, deletePost);
+
 router.get('/user-feed', verify, userFeed);
+router.post('/user-posts', userPosts);
 
 router.put('/like-post', verify, likePost);
 router.put('/unlike-post', verify, unlikePost);
 
 router.put('/add-comment', verify, addComment);
 router.put('/remove-comment', verify, removeComment);
+
+router.get('/all-posts', allPosts);
+
+//Admin:
+router.delete('/admin/delete-post/:id', verifyAdmin, deletePost);
 
 module.exports = router;
