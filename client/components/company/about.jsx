5a31a48567237f8b6dcb27d11bba8b00c839b2ef
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-16 21:33:14 +0530

    Create about.jsx

---
+++ client/components/company/about.jsx
@@ -0,0 +1,32 @@
+import AppFooter from "../../components/company/AppFooter"
+import Facts from "../../components/company/Facts"
+import Featured from "../../components/company/Featured"
+import Features from "../../components/company/Features"
+import Memories from "../../components/company/Memories"
+import styles from "../../styles/Company.module.css"
+
+const about = () => {
+    return (
+        <div className={styles.aboutWrapper}>
+            <span className={styles.bg}>
+                <span className={styles.title}>About Us</span>
+            </span>
+            <div className={styles.about}>
+                <div className={styles.aboutHead}>
+                    <span className={styles.aboutTitle}>Let's Connect from here!</span>
+                    <span className={styles.text}>
+                        The social media platform to 
+                        connect, explore, and share your ideas.
+                    </span>
+                </div>
+            </div>
+            <Featured />
+            <Features />
+            <Memories />
+            <Facts />
+            <AppFooter />
+        </div>
+    )
+}
+
+export default about
