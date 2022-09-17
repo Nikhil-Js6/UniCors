2ce1e455ce545a2f26e435616c6b0ea5712291d7
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-17 11:41:49 +0530

    Update People.js

--- client/components/cards/People.js
+++ client/components/cards/People.js
@@ -1,12 +1,26 @@
 import { useContext } from 'react'
 import { UserContext } from '../../context'
 import Link from 'next/link'
-import styles from '../../styles/Profile.module.css'
 import { SyncOutlined } from '@ant-design/icons'
+import styles from '../../styles/Profile.module.css'
+import axios from 'axios'
 
-const People = ({ user, handleFollow, handleUnfollow, loading }) => {
+const People = ({ user, messenger, setConversationId, handleFollow, handleUnfollow, loading }) => {
 
     const [state] = useContext(UserContext);
+
+    const handleConversation = async (recieverId) => {
+        try {
+            const conversation = await axios.post('/conversation', {
+                senderId: state.userInfo._id,
+                recieverId
+            });
+            setConversationId(conversation.data._id);
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <div className={styles.listUser}>
@@ -26,19 +40,33 @@ const People = ({ user, handleFollow, handleUnfollow, loading }) => {
                 </span>
             </span>
             {
-                user.followers.includes(state?.userInfo?._id) 
-                ? <span 
-                    className={styles.unfollow}  
-                    onClick={() => handleUnfollow(user)}
-                >
-                    { loading ? <SyncOutlined /> : 'Unfollow'}
-                </span>
-                : <span 
-                    className={styles.follow}  
-                    onClick={() => handleFollow(user)}
-                >
-                    { loading ? <SyncOutlined /> : 'Follow'}
-                </span>
+              !messenger ?
+                <>
+                {
+                    
+                    user.followers.includes(state?.userInfo?._id) 
+                    ? <span 
+                        className={styles.unfollow}  
+                        onClick={() => handleUnfollow(user)}
+                    >
+                        { loading ? <SyncOutlined /> : 'Unfollow'}
+                    </span>
+                    : <span 
+                        className={styles.follow}  
+                        onClick={() => handleFollow(user)}
+                    >
+                        { loading ? <SyncOutlined /> : 'Follow'}
+                    </span>
+                }
+                </> 
+                : <>
+                    <span 
+                        className={styles.follow}  
+                        onClick={() => handleConversation(user._id)}
+                    >
+                        { loading ? <SyncOutlined /> : 'Message'}
+                    </span>
+                </>
             }
         </div>
     )
