af0e4f6810639e02abe9ab51c94bc5a4ee5327b2
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-17 11:38:04 +0530

    Update Conversation.js

--- client/components/messenger/Conversation.js
+++ client/components/messenger/Conversation.js
@@ -1,27 +1,51 @@
 import { CheckOutlined } from '@ant-design/icons';
-import { useState, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { UserContext } from '../../context'
 import styles from '../../styles/Messenger.module.css'
+import axios from 'axios'
 
-const conversation = ({ conversation, setConversationId, conversation: { users } }) => {
+const conversation = ({ conversation, setConversationId }) => {
+
+    const [state] = useContext(UserContext);
+
+    const userId = conversation.members.filter(id => id !== state.userInfo._id);
+
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        if (state && state.token) {
+            getUsers();
+        }
+    }, [conversation.members]);
+
+    const getUsers = async () => {
+        try {
+            const user = await axios.post('/get-user', { _id: [userId] });
+            setUser(user.data);
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <div 
             className={styles.conversation} 
             onClick={() => {
                 setConversationId(conversation._id)
+                console.log(user);
             }}
         >
             <div className={styles.imageWrapper}>
-                <img className={styles.chatUserImg} src={users?.reciever?.image || '/noImage.jpg'}/>
+                <img className={styles.chatUserImg} src={user.image?.url || '/noImage.jpg'}/>
                 <span className={styles.online}></span>
             </div>
             <div className={styles.user}>
-                <div className={styles.username}>{users?.reciever?.name}</div>
-                <div className={styles.msg}>Hii...</div>
+                <div className={styles.username}>{user.name}</div>
+                <div className={styles.msg}>{'About User'}</div>
             </div>
             <div className={styles.msgIcons}>
-                <CheckOutlined style={{ color: '#1775e5' }}/>
+                <CheckOutlined style={{ color: '#1775e5'}}/>
             </div>
         </div>
     )
@@ -29,6 +53,17 @@ const conversation = ({ conversation, setConversationId, conversation: { users }
 
 export default conversation
 
+{/* <div className={styles.conversation}>
+    <div className={styles.imageWrapper}>
+        <img className={styles.chatUserImg} src={'/noImage.jpg'}/>
+        <span className={styles.online}></span>
+    </div>
+    <div className={styles.user}>
+        <div className={styles.username}>Username</div>
+        <div className={styles.typing}>typing...</div>
+    </div>
+    <span className={styles.msgCount}>3</span>
+</div> */}
 {/* <div className={styles.conversation}>
     <div className={styles.imageWrapper}>
         <img className={styles.chatUserImg} src={'/noImage.jpg'} />
