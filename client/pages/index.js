1d9de6ec0d7aa4d6443b9ec3ef2e70968966fe0a
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-18 16:53:22 +0530

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,91 +1,32 @@
-import { useState, useEffect, useContext } from 'react'
-import { UserContext } from '../context'
-import Head from 'next/head'
-import PostCard from '../components/cards/PostCard';
-import styles from '../styles/Home.module.css'
-import axios from 'axios';
-import io from 'socket.io-client';
+import AppFooter from "../components/company/AppFooter"
+import Facts from "../components/company/Facts"
+import Featured from "../components/company/Featured"
+import Features from "../components/company/Features"
+import Memories from "../components/company/Memories"
+import styles from "../styles/Company.module.css"
 
-const socket = io(process.env.NEXT_PUBLIC_SOCKETIO, {
-    reconnection: true,
-});
-
-const Home = ({ posts }) => {
-
-    const [state] = useContext(UserContext);
-
-    const [postFeed, setPostFeed] = useState([]);
-
-    useEffect(() => {
-        socket?.on('new-post', (newPost) => {
-            setPostFeed([newPost, ...posts]);
-        });
-        socket.on('post-deleted', (deletedPost) => {
-            console.log(deletedPost);
-            let newPosts = postFeed.filter((post, deletedPost) => {
-                return post._id !== deletedPost._id;
-            })
-            setPostFeed(newPosts);
-            console.log(newPosts);
-        });
-    }, []);
-
-    const collection = postFeed.length ? postFeed : posts;
-    
-    const head = () => (
-        <Head>
-            <title> UniCors - A social network by Developers.</title>
-            <meta 
-                name='description'
-                content='A social network for developers by developers'
-            />
-            <meta 
-                property='og:description'
-                content='A social network for developers by developers'
-            />
-            
-            <meta property='og:type' content='website'/>
-            <meta property='og:site_name'content='UniCors'/>
-            <meta property='og:url' content='https://unicors.com'/>
-            
-            <meta 
-                property='og:image:secure_url' 
-                content='https://unicors.com/images/noImage.com'
-            />
-        </Head>
-    ) 
-
+const About = () => {
     return (
-        <div className={styles.homeWrapper}>
-            { head() }
+        <div className={styles.aboutWrapper}>
             <span className={styles.bg}>
-                <span className={styles.title}>Home</span>
+                <span className={styles.title}>About Us</span>
             </span>
-            <div className={styles.cardWrapper}>
-                <PostCard page={'home'} userPosts={collection}/>
+            <div className={styles.about}>
+                <div className={styles.aboutHead}>
+                    <span className={styles.aboutTitle}>Let's Connect from here!</span>
+                    <span className={styles.text}>
+                        The social media platform to 
+                        connect, explore, and share your ideas.
+                    </span>
+                </div>
             </div>
+            <Featured />
+            <Features />
+            <Memories />
+            <Facts />
+            <AppFooter />
         </div>
     )
 }
 
-export const getServerSideProps = async () => {
-
-    const { data } = await axios.get('/all-posts');
-
-    if (data) {
-        return {
-            props: {
-                posts: data
-            }
-        }
-    } 
-    else {
-        return {
-            props: {
-                posts: null
-            }
-        }
-    }
-}
-
-export default Home
+export default About
