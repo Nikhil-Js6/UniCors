998c635610f996ed853979bd4571732eadc2be45
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-18 19:11:31 +0530

    Update Footer.js

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,12 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.copyright}>
-           <p>Made with ❤️ by <span className={styles.colored}><a href="https://nikhil-js6.netlify.app/" target="blank">Nikhil</a></span><span style={{color: 'gold', fontSize: '12px'}}>@UniCorus</span></p>
+            <p>Made with ❤️ by 
+                <span className={styles.colored}>
+                   <a href="https://nikhil-js6.netlify.app/" target="blank">Nikhil</a>
+                </span>
+                <span style={{color: 'gold', fontSize: '12px'}}>@Unicors</span> 
+            </p>
         </div>
     )
 }
