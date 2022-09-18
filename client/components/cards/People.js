96d709dee92f205d8fe815c80ad978ad106752eb
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-18 19:29:27 +0530

    Update People.js

--- client/components/cards/People.js
+++ client/components/cards/People.js
@@ -12,10 +12,10 @@ const People = ({ user, messenger, setConversationId, handleFollow, handleUnfoll
     const handleConversation = async (recieverId) => {
         try {
             const conversation = await axios.post('/conversation', {
-                senderId: state.userInfo._id,
+                senderId: state?.userInfo?._id,
                 recieverId
             });
-            setConversationId(conversation.data._id);
+            setConversationId(conversation?.data?._id);
         } 
         catch (err) {
             console.log(err);
@@ -27,14 +27,14 @@ const People = ({ user, messenger, setConversationId, handleFollow, handleUnfoll
             <span className={styles.peopleInfoWrapper}>
             { user.image
                 ? <span className={styles.peopleImgWrapper}>
-                    <img className={styles.peopleImg} src={user.image?.url}/>
+                    <img className={styles.peopleImg} src={user?.image?.url}/>
                 </span>
                 : <span className={styles.peopleImgWrapper}>
                     <img className={styles.peopleImg} src='/noImage.jpg' />
                 </span>
             }
                 <span className={styles.username}>
-                    <Link href={`/user/profile/${user._id}`}>
+                    <Link href={`/user/profile/${user?._id}`}>
                         {user.name}
                     </Link>
                 </span>
@@ -62,7 +62,7 @@ const People = ({ user, messenger, setConversationId, handleFollow, handleUnfoll
                 : <>
                     <span 
                         className={styles.follow}  
-                        onClick={() => handleConversation(user._id)}
+                        onClick={() => handleConversation(user?._id)}
                     >
                         { loading ? <SyncOutlined /> : 'Message'}
                     </span>
