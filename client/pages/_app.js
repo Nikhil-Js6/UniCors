e2101d0c7a382c26bb248facea392bd16a513742
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-09-18 16:50:41 +0530

    Update _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
       <Layout>
           <Head>
-               
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+            <link rel='icon' href='/icon.ico'/>
+            <link rel='apple-touch-icon' href='/icons/icon-192x192.png'/>
+            <link rel='manifest' href='/manifest.json'/>
           </Head>
           <Component {...pageProps} />
       </Layout>
